83acbb885797acc7703852662f54aaee1d27d8f1
Ihor Zhiwotowsky <zhiwotowsky.ihor@gmail.com>
2021-12-08 21:48:32 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+"use strict";//строгій режим, ставиться тільки в началі
+
+console.log('arr' + " - object");
+console.log(4 + +" 5");
+
+/*let incr = 10,
+    decr = 10;
+    
+incr++;    //увиличение на единичку
+decr--;    //Уминшение на единичку   
+
+console.log(incr);
+console.log(decr);*/
+
+let incr = 10,
+    decr = 10;
+    
+//++incr;    
+//--decr;     
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+
+const isChecked = false;
+      isClose = false;
+
+console.log(isChecked || !isClose);      
+      
+
+console.log(2 + 2 * 2 !== '6'); 
